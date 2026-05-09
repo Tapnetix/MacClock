dc08ffecb04d286556c7339d9ea2f63b99ca3b11
Yury Yudin <yuri.yudin@gmail.com>
2026-05-09 15:39:06 +0100

    refactor: move ThemeChangeModifier into Views/ (M6)
    A view-modifier helper used only by MainClockView doesn't belong in the
    @main app entry file. Moves struct ThemeChangeModifier and the View
    extension verbatim into Views/ThemeChangeModifier.swift.

--- MacClock/MacClockApp.swift
+++ MacClock/MacClockApp.swift
@@ -68,38 +68,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 }
 
-// MARK: - Theme Change Modifier
-
-struct ThemeChangeModifier: ViewModifier {
-    let settings: AppSettings
-    let dimManager: DimManager
-    let sunrise: Date?
-    let sunset: Date?
-
-    func body(content: Content) -> some View {
-        content
-            .onChange(of: settings.autoDimEnabled) { _, _ in update() }
-            .onChange(of: settings.autoDimMode) { _, _ in update() }
-            .onChange(of: settings.autoDimLevel) { _, _ in update() }
-            .onChange(of: settings.autoThemeEnabled) { _, _ in update() }
-            .onChange(of: settings.autoThemeMode) { _, _ in update() }
-            .onChange(of: settings.dayTheme) { _, _ in update() }
-            .onChange(of: settings.nightThemeAuto) { _, _ in update() }
-            .onChange(of: settings.colorTheme) { _, _ in update() }
-            .onChange(of: settings.nightTheme) { _, _ in update() }
-    }
-
-    private func update() {
-        dimManager.update(settings: settings, sunrise: sunrise, sunset: sunset)
-    }
-}
-
-extension View {
-    func onThemeSettingsChange(settings: AppSettings, dimManager: DimManager, sunrise: Date?, sunset: Date?) -> some View {
-        modifier(ThemeChangeModifier(settings: settings, dimManager: dimManager, sunrise: sunrise, sunset: sunset))
-    }
-}
-
 struct MainClockView: View {
     let settings: AppSettings
     let weatherService: WeatherService
